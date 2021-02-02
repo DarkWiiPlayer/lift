56229b7d16ec5244905173bb21407a0517d2dc4e
DarkWiiPlayer <darkwiiplayer@hotmail.com>
2021-02-02 09:55:59 +0100

    Add `bypass` function

--- lift/init.lua
+++ lift/init.lua
@@ -4,11 +4,15 @@ for _, method in pairs{"create", "isyieldable", "running", "status", "wrap"} do
 	lift[method] = coroutine[method]
 end
 
+--- Analyzes the return value of a yielding coroutine.
 local function intercept(co, success, target, ...)
+	-- Coroutine completed: Stay on current level
 	if coroutine.status(co) == "dead" then
 		return success, target, ...
-	elseif (not target) or target == coroutine.running() then
+	-- No target or matching level: Stay on current level
+	elseif (target == nil) or target == coroutine.running() then
 		return success, ...
+	-- Non-matching Target: lift through
 	else
 		return lift.resume(co, coroutine.yield(target, ...))
 	end
@@ -22,8 +26,17 @@ function lift.yield(...)
 	return coroutine.yield(nil, ...)
 end
 
+function lift.bypass(...)
+	if type(...) == "thread" or type(...) == "nil" then
+		error("First argument to bypass cannot be thread or nil")
+	end
+	return coroutine.yield(...)
+end
+
 function lift.yieldto(co, ...)
-	-- todo: type check co
+	if co ~= nil and type(co) ~= "thread" then
+		error("Can only yield to thread or nil")
+	end
 	return coroutine.yield(co, ...)
 end
 
